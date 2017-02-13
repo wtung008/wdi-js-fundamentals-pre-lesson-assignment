a5df3e05e11258fc9eeeb77580770e2cc8f6e999
wtung008 <wayne.h.tung@gmail.com>
2017-02-13 15:56:18 -0500

    Add objects to pacman-objects-2.js file

---
+++ pacman-objects-2.js
@@ -0,0 +1,10 @@
+var ghosts = {inky: 'Cyan', blinky: 'Red', pinky: 'Pink', clyde: 'Orange'};
+console.log(ghosts);
+console.log(ghosts.inky);
+console.log(ghosts.blinky);
+
+console.log(ghosts['inky']);
+console.log(ghosts['blinky']);
+
+var ghostName = 'clyde';
+console.log(ghosts[ghostName]);
